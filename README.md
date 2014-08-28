7276caece2b03919e581cd1f0a4c32885f1d3e74
Hamdan Javeed <hamdan@hamdanjaveed.com>
2014-08-27 23:39:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+Not-So-Annoying-Whitespace-Fixer
+================================
+
+A Brackets extension that trims trailing whitespace (except for the line the cursor is currently on).
+Also as a bonus it ensures the file has a new line at the end.
+
+
+Instructions
+============
+
+Just save the file and it'll do its thing.
+
+Install within Brackets
+=======================
+
+Click the extension manager button within Brackets (in the toolbar to the right) and search for "Not So Annoying Whitespace Fixer". Click install.
+
+
+Install from URL
+================
+
+1. Open the the Extension Manager from the File menu
+2. Copy paste the URL of the github repo or zip file
+
+
+Install from file system
+========================
+
+1. Download this extension using the ZIP button above and unzip it.
+2. Copy it in Brackets' `/extensions/user` folder by selecting `Help > Show Extension Folder` in the menu.
+3. Reload Brackets.
---
+++ main.js
@@ -0,0 +1,73 @@
+/*
+ * The Hamdan License
+ * Copyright (c) 2014 Hamdan Javeed. All rights reserved (yes, all of them).
+ *
+ * Do whatever. Don't be dumb.
+ * Credit appreciated.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, $, brackets */
+
+/** Not So Annoying Whitespace Fixer Extension
+    Trims trailing whitespace, but NOT the line the cursor is currently on.
+    Ensures that the last line is a \n.
+*/
+
+define(function (require, exports, module) {
+    'use strict';
+
+    var CommandManager = brackets.getModule('command/CommandManager');
+    var EditorManager = brackets.getModule("editor/EditorManager");
+    var DocumentManager = brackets.getModule("document/DocumentManager");
+
+    $(DocumentManager).on("documentSaved", function() {
+        var editor = EditorManager.getFocusedEditor();
+        var doc = DocumentManager.getCurrentDocument();
+        var cursorPos = editor.getCursorPos();
+
+        var text = doc.getText();
+        var linesToTrim = [];
+        var rawLines = text.split("\n");
+        var hasWhitespace = /\s+$/;
+
+        // go through all the lines and add the indices of the lines with whitespace (ignoring the line the cursor is on)
+        for (var i = 0; i < rawLines.length; i++) {
+            if (cursorPos.line !== i && hasWhitespace.test(rawLines[i])) linesToTrim.push(i);
+        }
+
+        doc.batchOperation(function() {
+            // if the text does not have a new line at the end
+            if (!/\n$/.test(text)) {
+                // append a new line to the end of the text
+                var lastLine = rawLines[rawLines.length - 1];
+                doc.replaceRange(lastLine + "\n", {
+                    line: rawLines.length - 1,
+                    ch: 0
+                }, {
+                    line: rawLines.length - 1,
+                    ch: lastLine.length
+                });
+            }
+
+            if (!linesToTrim.length) return;
+
+            // for each line that needs to be trimmed, trim it
+            for (var i = 0; i < linesToTrim.length; i++) {
+                var lineIndex = linesToTrim[i];
+
+                var startRange = {
+                    line: lineIndex,
+                    ch: 0
+                };
+
+                var endRange = {
+                    line: lineIndex,
+                    ch: rawLines[lineIndex].length
+                };
+
+                doc.replaceRange(rawLines[lineIndex].replace(/\s+$/, ''), startRange, endRange);
+            }
+        });
+    });
+});
